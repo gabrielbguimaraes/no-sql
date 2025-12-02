110cd4c141eab6c105787f9b8355d0d661256be9
gabrielbguimaraes <biel_google@hotmail.com>
2025-12-02 18:32:38 -0300

    adicionando zip do cassandra

--- atv4-cassandra/data_config/cassandra_init.py
+++ atv4-cassandra/data_config/cassandra_init.py
@@ -5,11 +5,11 @@ from data_config.collections_init import collections_init
 def connect_cassandra():
     try:
         cloud_config = {
-            'secure_connect_bundle': 'data_config/secure-connect-mercadolivre.zip'
+            'secure_connect_bundle': 'data_config/secure-connect-fatec-db.zip'
         }
         auth_provider = PlainTextAuthProvider(
             username='token', 
-            password='SEU TOKEN'
+            password='AstraCS:ujXhvNLQucxvEuBGpRIjkwEB:b18c13403d6f368f390376481997d4f62c38d94b942e828f0d2169d4e335568d'
         )
         cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
         session = cluster.connect()
