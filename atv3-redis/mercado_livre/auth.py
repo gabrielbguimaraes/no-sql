e0ba19d9c5ee051da51788349e48e93762055f2d
gabrielbguimaraes <biel_google@hotmail.com>
2025-12-02 17:12:10 -0300

    adaaaaaaaaaa

--- atv3-redis/mercado_livre/auth.py
+++ atv3-redis/mercado_livre/auth.py
@@ -1,21 +1,32 @@
-# auth.py
-from utils.utils import find_user
+def login(usuarios_collection, db_redis, email, password):
+    user = usuarios_collection.find_one({'email': email})
+    
+    if user:
 
-def login(usuarios_collection, db_redis, user_email, user_password):
-    user = find_user(user_email, usuarios_collection)
-    if user is None:
-        print('Usuário não encontrado!')
-        return None
-
-    if user_password == user['senha']:
-        user['_id'] = str(user['_id']) 
-        db_redis.setex(f'user:{user_email}', 3600, user['_id'])
-        print(f"Usuário {user_email} logado com sucesso por 1 hora!")
+        senha_banco = user.get('senha') 
+        
+        if senha_banco is None:
+            print(f"Aviso: O usuário {email} não tem senha cadastrada no banco.")
+            if password == "":
+                 pass 
+            else:
+                 print("Erro: Senha incorreta (ou usuário sem senha definida).")
+                 return None
+        elif senha_banco != password:
+            print("Senha incorreta!")
+            return None
+            
+        print(f"Login bem-sucedido para {user['nome']}")
+        # Adiciona usuário logado no Redis com TTL de 1 hora (3600s)
+        db_redis.setex(f"session:{email}", 3600, str(user['_id']))
         return user
     else:
-        print('Senha incorreta.')
+        print("Usuário não encontrado!")
         return None
 
-def user_logged(db_redis, user_email):
-    return bool(db_redis.get(f'user:{user_email}'))  # Retorna True se existir, False caso contrário
+def logout(db_redis, email):
+    db_redis.delete(f"session:{email}")
+    print("Logout realizado com sucesso!")
 
+def user_logged(db_redis, email):
+    return db_redis.exists(f"session:{email}")
